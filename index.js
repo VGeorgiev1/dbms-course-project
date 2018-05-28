47583c9e9c6d95931d31071dc2d4a70340f06784
Vladislav <vladdislav.georgiev@gmail.com>
2018-05-28 13:11:57 +0300

    adding sample registration

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const connection = require('./db_connection.js').connection
+const bodyParser = require('body-parser');
+const regcontrollers = require('./controllers/register');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.set('view engine', 'pug')
+app.use(express.static('public'))
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
+app.get('/', (req, res) => res.render('register'))
+app.post('/register', function(req,res){regcontrollers.utils.register(req,res,connection)})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
