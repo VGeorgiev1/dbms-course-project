5436d9d539740e292d2798e6e9db2653135b035e
mimi5025 <mstoanova0@gmail.com>
2018-06-07 19:56:50 +0300

    fixed recipe view

--- index.js
+++ index.js
@@ -21,21 +21,17 @@ var loginware = function (req, res, next) {
     
 }
 app.use(loginware)
-app.get('/recipe/:id', (req, res) =>{
+app.get('/recipe/view/:id', (req, res) =>{
     var recipe = Recipe.find_by({id: req.params.id}).then(result =>{
         console.log(result)
-        res.render('recipe', {"recipe": result})
+        res.render('recipe', {"recipe": result[0]})
     })
 })
 app.get('/', (req,res) =>{
     if(!req.username) res.redirect('/login')
     else{
-<<<<<<< HEAD
-        var recipe = Recipe.find_all().then(result =>{
-=======
         var recepy = Recipe.find_all().then(result =>{
             console.log(result)
->>>>>>> d218620c35d10c252453a74fe8f676356c6ec9a2
             res.render('index', {"recipes": result})
         })
     }
